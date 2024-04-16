bc61a929d3c92026659dccbe55ff25362383ebb1
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2024-04-16 11:34:45 +0530

    New Commit

--- React_FireBase/src/App.jsx
+++ React_FireBase/src/App.jsx
@@ -1,5 +1,5 @@
 import { app } from "./Firebase";
-import { getAuth,createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth,signInWithEmailAndPassword } from "firebase/auth";
 import './App.css'
 import { useState } from "react";
 
@@ -9,7 +9,9 @@ function App() {
   const[password,setPassword] = useState("");
 
   const putData = ()=>{
-    createUserWithEmailAndPassword(auth,email,password);
+    signInWithEmailAndPassword(auth,email,password).then(()=>{
+      alert("Sign SuccesFull");
+    }).catch(()=>{alert("You are Entering wrong Email and password")});
   }
   return (
     <>
@@ -18,7 +20,7 @@ function App() {
     <input type="email" name="" id="" onChange={(e)=>setEmail(e.target.value)}/>
     <h3>Password</h3>
     <input type="password" name="" id="" onChange={(e)=>setPassword(e.target.value)}/><br/>
-    <button onClick={putData}>Sign Up</button>
+    <button onClick={putData}>Sign In</button>
     </>
   )
 }
